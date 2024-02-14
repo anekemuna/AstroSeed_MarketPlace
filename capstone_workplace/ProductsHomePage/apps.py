0200d6dbf1cc8d18012abba407dac1b452bb22e1
Al-Faisal Al-Maawali <akalmaaw@asu.edu>
2024-02-14 11:45:37 -0700

    delet

--- capstone_workplace/ProductsHomePage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductshomepageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ProductsHomePage'
--- capstone_workplace/login/views.py
+++
@@ -1,6 +0,0 @@
-# login/views.py
-
-from django.shortcuts import render
-
-def role_selection(request):
-    return render(request, 'login/role_selection.html')
