99bc23ab7de86c6d1393da857f6eb1a80284496f
Al-Faisal Al-Maawali <akalmaaw@asu.edu>
2024-03-25 18:30:32 -0700

    adding seller page in views

--- AstroSeed_MarketPlace-code-folder/capstone_workplace/ProductsHomePage/views.py
+++ AstroSeed_MarketPlace-code-folder/capstone_workplace/ProductsHomePage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def product_page(request):
     return render(request, 'ProductsHomePage/homepage.html')
+
+def product_page(request):
+    return render(request, 'ProductsHomePage/SellerHomepage.html')
