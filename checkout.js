a0b473ddc7475031e14d2ca64d2282d1cf72055c
SasankChowdaryA <117131348+SasankChowdaryA@users.noreply.github.com>
2024-03-31 22:00:45 -0700

    Revert "Checkout page"

--- checkout.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const checkoutBtn = document.getElementById("checkout-btn");
-
-    checkoutBtn.addEventListener("click", function() {
-        // Perform checkout logic here
-        alert("Checkout successful!");
-    });
-});
