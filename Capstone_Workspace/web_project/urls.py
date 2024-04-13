fe7ac51af75cb1ad0f239ecec6e38d1160705c22
Al-Faisal Al-Maawali <akalmaaw@asu.edu>
2024-04-13 08:19:03 -0700

    Adding product feature

--- Capstone_Workspace/web_project/urls.py
+++ Capstone_Workspace/web_project/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 
+#Alfaisal
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,7 @@ urlpatterns = [
     #path('', include('ProductsHomePage.urls'))
     path('', include('Marketplace.urls')),
 ]
+
+#Alfaisal
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
